b5ae36b0310e365db04de2127ecb946fd2bf515b
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-01-10 23:12:10 +0000

    if statement

--- python-crash-course/chapter5/cars.py
+++ python-crash-course/chapter5/cars.py
@@ -1,10 +1,11 @@
 ## IF STATEMENTS
 
 # A simple example
-cars = ['audi', 'bwm', 'subaru', 'toyota']
+cars = ['audi', 'bmw', 'subaru', 'toyota']
 for car in cars:
     if car == 'bmw':
         print(car.upper())
     else:
         print(car.title())
     
+# at the heart of every if statement is an expression that can be evaluated as True or False and is called conditional test.

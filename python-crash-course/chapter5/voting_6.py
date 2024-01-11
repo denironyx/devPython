71d6398d5aa69be8cd94d7f27b508e39914ef7dc
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-01-11 18:59:13 +0000

    voting

---
+++ python-crash-course/chapter5/voting_6.py
@@ -0,0 +1,17 @@
+## If statements
+# If the conditional test evalauates to True, python executes the code following the if statement
+# If the test evaluates to False, Python ignores the code following the if statement
+
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+
+# if-else statements
+
+age = 17
+if age >= 18:
+    print("You are old enough to  vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too youngg to vote.")
+    print("Please register to vote as soon as you turn 18!")

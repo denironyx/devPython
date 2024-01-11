495c4764917e1a4d39cc6b49f487ded662e1ea70
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-01-11 19:15:38 +0000

    toppiings

--- python-crash-course/chapter5/toppings.py
+++ python-crash-course/chapter5/toppings.py
@@ -19,3 +19,17 @@ if 'pepperoni' in requested_toppings:
     print('Adding pepperoni.')
 if 'extra cheese'  in requested_toppings:
     print('Addinig extra cheese.')   
+
+## Using multiple Lists
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+        
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+        
+print("\nFinished making your pizza!")

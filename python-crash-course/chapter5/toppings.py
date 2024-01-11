8c66d939a2477393896450534494ffab1e76928f
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-01-11 19:09:11 +0000

    toppings

--- python-crash-course/chapter5/toppings.py
+++ python-crash-course/chapter5/toppings.py
@@ -7,3 +7,15 @@ if requested_toppings != 'anchovies':
     
 if requested_toppings != 'mushrooms':
     print("Hold the anchovies!")
+
+# Testing Multiple Conditions
+# The if-elif-else chain is powerful, butt it's only appropriate to use when you just need one test to pass.
+
+requested_toppings = ['mushrooms','extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print('Adding pepperoni.')
+if 'extra cheese'  in requested_toppings:
+    print('Addinig extra cheese.')   

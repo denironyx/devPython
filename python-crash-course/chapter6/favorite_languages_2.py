410f59370c3be1c8643c1d3b6b33e408b2be11b0
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-01-12 21:07:08 +0000

    similar object

---
+++ python-crash-course/chapter6/favorite_languages_2.py
@@ -0,0 +1,10 @@
+## Dictionary of similar objects
+favourite_languages = {
+    'jen': 'python',
+    'sarah': 'c', 
+    'edward': 'ruby',
+    'phil': 'python',
+} # leaving a comma at the end so, I am ready to add new value or pair
+
+language = favourite_languages['sarah'].title()
+print(f"Sarah's favourite language is {language}.")

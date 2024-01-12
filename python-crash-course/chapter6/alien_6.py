d8797a41d89427f2b48221bed582ee49794ce397
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-01-12 07:09:52 +0000

    dictionaries

---
+++ python-crash-course/chapter6/alien_6.py
@@ -0,0 +1,12 @@
+## Dictionaires
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+print(alien_0['color'])
+print(alien_0['points'])
+
+#  Working with dictionaire 
+# dictionary is a collection of key-value pairs. Each key is connected to a value.
+
+new_points = alien_0['points']
+print(f"You just earned {new_points} points")

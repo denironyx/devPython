3cee9bd39f2067c194397d54c125345b07a97038
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-01-18 04:12:57 +0000

    chapter 8

--- python-crash-course/chapter8/greet.py
+++ python-crash-course/chapter8/greet.py
@@ -1,5 +1,5 @@
-def greet_user():
+def greet_user(username):
     """Display a simple greetings"""
-    print("Hello!")
+    print(f"Hello, {username.title()}!")
     
-greet_user()
+greet_user("jesse")

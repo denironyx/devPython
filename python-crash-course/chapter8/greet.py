adbcb8b105aa0c0740484fecf45fde5a3eb4c00e
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-01-18 07:24:52 +0000

    return values and function

--- python-crash-course/chapter8/greet.py
+++ python-crash-course/chapter8/greet.py
@@ -1,5 +1,18 @@
-def greet_user(username):
-    """Display a simple greetings"""
-    print(f"Hello, {username.title()}!")
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted"""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+## This is an infinite loop!
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
     
-greet_user("jesse")
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}")

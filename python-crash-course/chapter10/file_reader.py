9c6eb32526a928de2a93c76912832004a895f1bb
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-01-30 21:46:46 +0000

    file reader list of lines

--- python-crash-course/chapter10/file_reader.py
+++ python-crash-course/chapter10/file_reader.py
@@ -1,3 +1,16 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-print(contents.rstrip())
+file_name = 'pi_digits.txt'
+
+# with open(file_name) as file_object:
+#     contents = file_object.read()
+# print(contents.rstrip())
+
+# with open(file_name) as file_object:
+#     for line in file_object:
+#         print(line.rstrip())
+
+## Making a list of lines from a file
+with open(file_name) as file_object:
+    lines = file_object.readlines() # he readlines() method takes each line from the file and stores it
+    
+for line in lines: #e use a simple for loop to print each line from lines
+    print(line.rstrip())

4bc9238d8c50864ddabf5058f35ca29761ddb060
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-02-07 21:53:53 +0000

    the setup method

--- python-crash-course/chapter11/test_survey.py
+++ python-crash-course/chapter11/test_survey.py
@@ -3,6 +3,13 @@ from survey import AnonymousSurvey
 
 class TestAnonymousSurvey(unittest.TestCase):
     """Tests for the class AnonymousSurvey"""
+    
+    def setUp(self):
+        """Create a survey and a set of responses for use in all test methods."""
+        question = "What language did you first learn to speak?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Spanish', 'Mandarin']
+    
     def test_store_single_response(self):
         """Test that a single response is store properly."""
         question = "What language did you first learn to speak?"

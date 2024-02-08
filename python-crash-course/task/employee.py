ea0c36c4ce8689f224a7324e119f8b2c8cbf1707
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-02-08 21:35:12 +0000

    employee script

---
+++ python-crash-course/task/employee.py
@@ -0,0 +1,17 @@
+# class Employee:
+#     """user names"""
+
+## Using try and except
+print("Employee raise amount")
+print("Enter 'q' to quit.")
+
+while True:
+    first_name = input("\nFirst name: ")
+    if first_name == 'q':
+        break
+    last_name = input("\nLast name: ")
+    if last_name == 'q':
+        break
+    amount = input("\nSalary: ")
+    if amount == 'q':
+        break

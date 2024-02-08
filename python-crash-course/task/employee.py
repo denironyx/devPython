7105c7244d46088f1d2b259ee10a56bf8c1092a6
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-02-08 21:48:24 +0000

    simple compensation

--- python-crash-course/task/employee.py
+++ python-crash-course/task/employee.py
@@ -5,13 +5,27 @@
 print("Employee raise amount")
 print("Enter 'q' to quit.")
 
-while True:
-    first_name = input("\nFirst name: ")
-    if first_name == 'q':
-        break
-    last_name = input("\nLast name: ")
-    if last_name == 'q':
-        break
-    amount = input("\nSalary: ")
-    if amount == 'q':
-        break
+class Employee:
+    """A simple employee compensation scripts"""
+    def __init__(self,first, last, salary):
+        self.first_name = first
+        self.last_name = last
+        self.annual_salary = salary
+        
+    def get_employee_name(self):
+        """Return employees name"""
+        full_name = f"{self.first_name} {self.last_name}"
+        return full_name
+    
+    def get_salary(self):
+        """Return salary"""
+        salary = self.annual_salary
+        return salary
+    
+    def give_raise(self, raises):
+        """Add the given amount to the annual salary"""
+        self.annual_salary += raises
+        print(f"5,000 annual increase")
+
+employees = Employee("Dennis", "Irorere", 5000)
+print(employees.get_employee_name())

c995f648d49ceeff0f3227be306d51aa228246cf
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-02-08 21:22:15 +0000

    test survey

--- python-crash-course/chapter11/test_survey.py
+++ python-crash-course/chapter11/test_survey.py
@@ -12,10 +12,15 @@ class TestAnonymousSurvey(unittest.TestCase):
     
     def test_store_single_response(self):
         """Test that a single response is store properly."""
-        question = "What language did you first learn to speak?"
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('English')
-        self.assertIn('English', my_survey.responses)
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
+
+    def test_store_three_responses(self):
+        """Test that three individual responses are stored properly."""
+        for response in self.responses:
+            self.my_survey.store_response(response)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
         
 if __name__ == '__main__':
     unittest.main()

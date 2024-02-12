64326b97904a919e482a9f3146da956ac52e44e4
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-02-12 21:06:37 +0000

    walk functions

--- python-crash-course/project/generating_data/random_walk.py
+++ python-crash-course/project/generating_data/random_walk.py
@@ -19,3 +19,21 @@ class RandomWalk:
         while len(self.x_values) < self.num_points:
             
             # Decide which direction to go and how far to go in that direction
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+            
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+            
+            # rejects moves that go nowhere.
+            if x_step == 0  and y_step == 0:
+                continue
+            
+            ## calculate the new position
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+            
+            self.x_values.append(x)
+            self.y_values.append(y)

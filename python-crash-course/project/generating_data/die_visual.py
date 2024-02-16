da368b7e13a9e963fd7bbada76b71ee0a32b4ef4
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-02-16 23:12:36 +0000

    rolling dice of different sizes

--- python-crash-course/project/generating_data/die_visual.py
+++ python-crash-course/project/generating_data/die_visual.py
@@ -2,15 +2,15 @@ from plotly.graph_objs import Bar, Layout
 from plotly import offline
 from die import Die
 
-# Create a D6
-die = Die()
-
+# Create a D6 and a D10
+die_1 = Die()
+die_2 = Die(10)
 
 # Make some rolls, and store results in a list.
 results = []
 
 for roll_num in range(100):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze the results.
@@ -26,7 +26,7 @@ data = [Bar(x=x_values, y=frequencies)]
 x_axis_config = {'title': 'Result'}
 y_axis_config = {'title': 'Frequency of Result'}
 
-my_layout = Layout(title='Results of rolling one D6 1000 times',
+my_layout = Layout(title='Results of rolling one D6 and D10 50000 times',
                    xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout}, filename = 'd6.html') 
     

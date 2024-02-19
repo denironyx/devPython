cfc6aee812f155d1fcf8103b73d31d50d1c99c00
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-02-19 13:51:59 +0000

    death valley highs low error catching

--- python-crash-course/project/download_data/death_valley_highs_lows.py
+++ python-crash-course/project/download_data/death_valley_highs_lows.py
@@ -16,11 +16,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[4])
-        low = int(row[5])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 print(highs)
 
